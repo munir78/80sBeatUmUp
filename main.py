4ed7ef61ac06251bfd79e645d28689cf025c8811
munir78 <mdog335@gmail.com>
2017-07-05 14:43:51 +0100

    Created a template

--- main.py
+++ main.py
@@ -1,4 +1,39 @@
-import math
 
-def retardOnline():
-    pass
+import pygame
+
+WIDTH = 360
+HEIGHT = 480
+FPS = 60
+
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# initialize pygame and create window
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("My Game")
+clock = pygame.time.Clock()
+
+all_sprites = pygame.sprite.Group()
+# Game loop
+while 1:
+    # keep loop running at the right speed
+    clock.tick(FPS)
+    # Process input (events)
+    for event in pygame.event.get():
+        # check for closing window
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
+    # Update
+    all_sprites.update()
+    # Draw / render
+    screen.fill(BLACK)
+    # *after* drawing everything, flip the display
+    all_sprites.draw(screen)
+    pygame.display.flip()
