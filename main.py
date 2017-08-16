a2d1bd9e91d6aaa32dcc9eb5c871a5852f0addf6
munir78 <mdog335@gmail.com>
2017-08-16 18:38:12 +0100

    Added concept of velocity and friction. Also separated files to streamline code

--- main.py
+++ main.py
@@ -1,74 +1,66 @@
-import pygame
-import os
+import pygame as pg
+import random
 from settings import *
+from sprites import *
 
-#asset folder
-game_folder = os.path.dirname(__file__) #the folder in which this file is in
-img_folder = os.path.join(game_folder, "img") # location of img assets
-
-# initialize pygame and create window
-pygame.init()
-pygame.mixer.init() #gives audio functionality
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Game")
-clock = pygame.time.Clock()
-
-class Player(pygame.sprite.Sprite):
-    # sprite for the Player
+class Game:
     def __init__(self):
-        # this line is required to properly create the sprite
-        pygame.sprite.Sprite.__init__(self)
-        # create a plain rectangle for the sprite image  self.image = pygame.Surface((50, 50))
+        # initialize game window, etc
+        pg.init()
+        pg.mixer.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption(TITLE)
+        self.clock = pg.time.Clock()
+        self.running = True
 
-        self.image = pygame.image.load(os.path.join(img_folder, "p1_jump.png")).convert()
-        self.image.set_colorkey(BLACK) # makes this colour transparent
-        # find the rectangle that encloses the image
-        self.rect = self.image.get_rect()
-        # center the sprite on the screen
-        self.rect.centerx = WIDTH - 750
-        self.rect.bottom = HEIGHT -10
-        self.speedx = 0
+    def new(self):
+        # start a new game
+        self.all_sprites = pg.sprite.Group()
+        self.player = Player()
+        self.all_sprites.add(self.player)
+        self.run()
 
+    def run(self):
+        # Game Loop
+        self.playing = True
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
 
     def update(self):
-        # any code here will happen every time the game loop updates
-        self.speedx = 0
-        keystate = pygame.key.get_pressed()
-        if keystate[pygame.K_RIGHT]:
-            self.speedx = 5
-        if keystate[pygame.K_LEFT]:
-            self.speedx = -5
-        self.rect.x += self.speedx
-        if self.rect.right > WIDTH:
-            self.rect.right = WIDTH
-        if self.rect.left < 0:
-            self.rect.left = 0
+        # Game Loop - Update
+        self.all_sprites.update()
 
+    def events(self):
+        # Game Loop - events
+        for event in pg.event.get():
+            # check for closing window
+            if event.type == pg.QUIT:
+                if self.playing:
+                    self.playing = False
+                self.running = False
 
+    def draw(self):
+        # Game Loop - draw
+        self.screen.fill(BLACK)
+        self.all_sprites.draw(self.screen)
+        # *after* drawing everything, flip the display
+        pg.display.flip()
 
+    def show_start_screen(self):
+        # game splash/start screen
+        pass
 
-all_sprites = pygame.sprite.Group()
-player = Player()
-all_sprites.add(player)
-# Game loop
-running = True
-while running:
-    # keep loop running at the right speed
-    clock.tick(FPS)
-    # Process input (events)
+    def show_go_screen(self):
+        # game over/continue
+        pass
 
-    for event in pygame.event.get():
-        # check for closing window
-        if event.type == pygame.QUIT:
-            running = False
+g = Game()
+g.show_start_screen()
+while g.running:
+    g.new()
+    g.show_go_screen()
 
-    # Update
-    all_sprites.update()
-
-    # Draw / render
-    screen.fill(BLUE)
-    all_sprites.draw(screen)
-    # *after* drawing everything, flip the display
-    pygame.display.flip()
-
-pygame.quit()
+pg.quit()
--- settings.py
+++ settings.py
@@ -1,7 +1,11 @@
-
-WIDTH = 800
+TITLE = "Muay Thai Fighter"
+WIDTH = 480
 HEIGHT = 600
 FPS = 60
+
+# Player properties
+PLAYER_ACC = 0.5
+PLAYER_FRICTION = -0.12
 
 # define colors
 WHITE = (255, 255, 255)
@@ -9,4 +13,4 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
-
+YELLOW = (255, 255, 0)
